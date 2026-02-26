d010fd7a1cdf3d8be844407a6e1658f8d1407906
dudua9356-beep <dudua9356@gmail.com>
2026-02-26 13:35:06 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,51 @@
-from flask import Flask
+from flask import Flask, render_template, redirect
+import requests
 
 app = Flask(__name__)
 
+# Usuário fake temporário
+class User:
+    def __init__(self):
+        self.id = 1
+        self.balance = 10000.0
+
+user = User()
+
 @app.route("/")
 def home():
-    return "VaultX Online 🚀"
+    return redirect("/dashboard")
+
+@app.route("/dashboard")
+def dashboard():
+    try:
+        response = requests.get(
+            "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd",
+            timeout=5
+        )
+        data = response.json()
+        btc_price = data["bitcoin"]["usd"]
+    except:
+        btc_price = "Indisponível"
+
+    return render_template(
+        "dashboard.html",
+        user=user,
+        btc_price=btc_price
+    )
+
+@app.route("/buy")
+def buy():
+    user.balance -= 100
+    return redirect("/dashboard")
+
+@app.route("/sell")
+def sell():
+    user.balance += 100
+    return redirect("/dashboard")
+
+@app.route("/admin-9482")
+def admin():
+    return "Área Admin"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
