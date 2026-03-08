dc9f0348525632643e57639f6ce7dc3001e96f07
dudua9356-beep <dudua9356@gmail.com>
2026-03-08 12:47:06 -0300

    Update User model fields and constraints

--- models.py
+++ models.py
@@ -3,8 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = "user"
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.String(5), unique=True)
-    username = db.Column(db.String(100))
-    balance = db.Column(db.Float, default=10000.0)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    balance = db.Column(db.Float, default=1000)
+
+    def __repr__(self):
+        return f"<User {self.username}>"
