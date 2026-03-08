86fff234c5741d48834a493f4dd3c79e57560167
dudua9356-beep <dudua9356@gmail.com>
2026-03-08 13:14:01 -0300

    Refactor user creation and update comments
    Refactor user creation logic to check for username before adding to the database. Update comments to Portuguese.

--- app.py
+++ app.py
@@ -12,8 +12,9 @@ app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "vaultx123")
 # INIT DB
 db.init_app(app)
 
-# CREATE TABLES
+# RESET DATABASE (remove tabelas antigas com erro)
 with app.app_context():
+    db.drop_all()
     db.create_all()
 
 # HOME
@@ -22,19 +23,19 @@ def home():
     users = User.query.all()
     return render_template("index.html", users=users)
 
-# CREATE USER
+# CRIAR USUÁRIO
 @app.route("/create", methods=["POST"])
 def create_user():
     username = request.form.get("username")
 
-    user = User(username=username, balance=1000)
-
-    db.session.add(user)
-    db.session.commit()
+    if username:
+        user = User(username=username, balance=1000)
+        db.session.add(user)
+        db.session.commit()
 
     return redirect("/")
 
-# ADD BALANCE
+# ADICIONAR SALDO
 @app.route("/add/<int:user_id>")
 def add_balance(user_id):
     user = User.query.get(user_id)
