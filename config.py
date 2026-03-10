2493eabcf744f08edbcfc7a4866b57051670d174
dudua9356-beep <dudua9356@gmail.com>
2026-03-10 10:29:41 -0300

    Update environment variable retrieval method

--- config.py
+++ config.py
@@ -1,7 +1,5 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.environ.get("SECRET_KEY","vaultxsecret")
-    ADMIN_PASSWORD = "duduzin321@321"
