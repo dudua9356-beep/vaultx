a2015dd7d4c041315b9f74605130af302c4cbaa0
dudua9356-beep <dudua9356@gmail.com>
2026-03-10 10:29:14 -0300

    Refactor User model fields and defaults

--- models.py
+++ models.py
@@ -2,12 +2,11 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
-    __tablename__ = "user"
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), unique=True, nullable=False)
-    balance = db.Column(db.Float, default=1000)
 
-    def __repr__(self):
-        return f"<User {self.username}>"
+    username = db.Column(db.String(50), unique=True)
+
+    balance = db.Column(db.Float, default=0)
